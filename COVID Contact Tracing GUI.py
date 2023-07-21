62f56ca16132e32beadf42b72f582c998bc47774
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-07-21 23:54:53 +0800

    Pseudocode

--- COVID Contact Tracing GUI.py
+++ COVID Contact Tracing GUI.py
@@ -2,3 +2,32 @@ import tkinter as tk
 from tkinter import messagebox
 from COVID_Contact_Tracing_App import ContactTacingApp
 
+#Create add entry and search entry buttons plus the grid
+#Create the labels, entry, and grid fields
+#Name
+#Date of Birth
+#Gender
+#Phone Number
+#Email Address
+#Address
+#Contact Person Name
+#Contact Person Phone Number
+#Contact Person Email Address
+#Relationship to the contact person
+#Date today
+#Time
+#Create a list to store the StringVar variables for the radio button sets
+#If vaccinated already
+#If experiencing any symptoms
+#If had been recently exposed to a suspected or confirmed case
+#If had been engaged with somebody who maybe exhibiting symptoms
+#If have been undergone a Covid-19 test
+#If have been undergone a self quarantine
+#Create an instance of the contact tracing app
+#Method to handle add_entry 
+#Get all the input values from the entry fields and radiobutton variables
+#Method to show the message box 
+#Method to handle search_entry 
+#From the ContactTracingApp class, call all the search_entry method and message box
+#Clear all entry fields and reset the radio buttons
+#Create the main window
