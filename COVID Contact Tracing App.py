db2d0463430064502724c05c304fd3d5c01be807
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-07-21 23:11:18 +0800

    ContactTracingApp class

--- COVID Contact Tracing App.py
+++ COVID Contact Tracing App.py
@@ -2,7 +2,12 @@
 #BSCPE 1-4
 #COVID Contact Tracing App
 
+import csv
+
 #Initialize ContactTracingApp class
+class ContactTacingApp:
+    def __init__(self):
+        pass
 #Write all the information that has been collected to the CSV file
 #From the CSV file, read all the entries and then search for the name 
 #Check to see if the name matches, then print whether or not the entry was found
