a7f0997bd1043eb46b0f91bb1ddd4d50b0938220
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-07-21 23:09:42 +0800

    Pseudocode

--- COVID Contact Tracing App.py
+++ COVID Contact Tracing App.py
@@ -1,3 +1,9 @@
 #Alexza Jean R. Catanoy
 #BSCPE 1-4
 #COVID Contact Tracing App
+
+#Initialize ContactTracingApp class
+#Write all the information that has been collected to the CSV file
+#From the CSV file, read all the entries and then search for the name 
+#Check to see if the name matches, then print whether or not the entry was found
+#Create a list to store the selected values from the radio button sets 
